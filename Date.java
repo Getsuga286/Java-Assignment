f5877881adb55fbaeedc7452062e70eda92a20e5
Getsuga <kaminari.count286@gmail.com>
2016-08-10 18:06:32 +0530

    Adding Files

---
+++ Date.java
@@ -0,0 +1,40 @@
+package helloworldapp;
+
+public class Date {
+	
+	public int month;
+	public int day;
+	public int year;
+	public Date(){
+		month = 0;
+		day = 0;
+		year = 0;
+	}
+	public Date(int mm, int dd, int yy){
+		month = mm;
+		day = dd;
+		year = yy;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public void displayDate(){
+		System.out.println(month + "/" + day + "/" + year);
+	}
+
+}
