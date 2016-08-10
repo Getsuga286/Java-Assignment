6ba4feb3c5c0651b737fb1302b69dcbea3749871
Getsuga <kaminari.count286@gmail.com>
2016-08-10 18:06:41 +0530

    Adding Files

---
+++ DateTest.java
@@ -0,0 +1,22 @@
+package helloworldapp;
+
+import java.util.Scanner;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input month - ");
+		int month = input.nextInt();
+		System.out.print("Input month - ");
+		int day = input.nextInt();
+		System.out.print("Input month - ");
+		int year = input.nextInt();
+		System.out.print("\nCurrent date - ");
+		Date date = new Date(month, day, year);
+		date.displayDate();
+		input.close();	
+	}
+
+}
