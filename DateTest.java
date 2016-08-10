d32653ceda2d98f431e161349c01e6f117b8a075
Getsuga286 <kaminari.count286@gmail.com>
2016-08-10 22:20:15 +0530

    Update DateTest.java

--- DateTest.java
+++ DateTest.java
@@ -9,9 +9,9 @@ public class DateTest {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input month - ");
 		int month = input.nextInt();
-		System.out.print("Input month - ");
+		System.out.print("Input day - ");
 		int day = input.nextInt();
-		System.out.print("Input month - ");
+		System.out.print("Input year - ");
 		int year = input.nextInt();
 		System.out.print("\nCurrent date - ");
 		Date date = new Date(month, day, year);
